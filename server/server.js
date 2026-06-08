68f626dc2929ab959cca5c044e78f91d6cd04446
Beatrice Olaosebikan <bto334@cs.utexas.edu>
2026-06-08 14:44:51 -0500

    debugged, client connection must be the same as port server is listening on, dynamically mapped gift json to render as cards on homepage

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ app.get('/',(req, res) =>
 )
 
 /*Start a server at a specified port and listen for requests from external client*/
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log('Server listening on http://localhost:',PORT)
 })
