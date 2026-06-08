2d07a5ea4f2c2ff775b028fbac6b8d4d0cd1b7e2
Beatrice Olaosebikan <bto334@cs.utexas.edu>
2026-06-07 19:16:37 -0500

    set up express server

---
+++ server/server.js
@@ -0,0 +1,19 @@
+import express from 'express'
+const app = express()
+/*middleware function that  serve static files from various client directory
+essentially allowing an external client to read the data from my local client*/
+app.use('/public', express.static('./public'))
+app.use('/scripts', express.static('./public/scripts'))
+/*Route for the root url
+Route - specific url in webpage paired with a http method to perform an action*/
+app.get('/',(req, res) => 
+{
+    res.status(200).send('<h1 style="text-align: center; margin-top: 50px;">UnEarthed API</h1>')
+}
+)
+
+/*Start a server at a specified port and listen for requests from external client*/
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+    console.log('Server listening on http://localhost:',PORT)
+})
